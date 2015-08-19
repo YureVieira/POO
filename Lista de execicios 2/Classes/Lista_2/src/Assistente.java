a333b840c582d508a6df85d3a80475f327fe2cc6
unknown <tecnicoyure@hotmail.com>
2015-08-19 13:26:24 -0300

    lista2

---
+++ Lista de execicios 2/Classes/Lista_2/src/Assistente.java
@@ -0,0 +1,11 @@
+
+public class Assistente extends Funcionario {
+	long matricula;
+	public Assistente() {
+		// TODO Auto-generated constructor stub
+	}
+	void exibeDados(){
+		System.out.println("O funcionario de nome "+nome+" matrinula nº "+matricula+" recebe salario de R$"+salario);
+	}
+
+}
